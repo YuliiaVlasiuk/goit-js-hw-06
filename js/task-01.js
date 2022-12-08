c85cf2e0980d558bc9b76d4b31de379c4484ae8c
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-08 22:19:17 +0200

    08.12

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,10 @@
+const listItemsOfCategories = document.querySelectorAll('.item');
+console.log("Number of categories:", listItemsOfCategories.length);
 
-const listItems = document.querySelectorAll('.item');
-console.log("Number of categories:",listItems.length);
-
-const listH2 = document.querySelectorAll('h2');
-
-listH2.forEach((itemH2) => {
-    console.log(itemH2.textContent);
+listItemsOfCategories.forEach((listItem) => {
+    console.log("Category:",listItem.firstElementChild.textContent);
+    
+    const itemsCounting = listItem.lastElementChild.querySelectorAll('li').length;
+    console.log("Eleents:",itemsCounting)
   });
 
-const children=listItems.children;
-  console.log(children)
-
-
-
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,34 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listOfImage=document.querySelector('.gallery');
+console.log(listOfImage);
+
+ console.log(images);
+
+// let markup='';
+// console.log(markup);
+
+// console.log(ingredients.length);
+// for (let i = 0; i < ingredients.length; i+=1) {
+// markup += `<li>${ingredients[i]}</li>`
+ 
+// }
+
+// // console.log(markup);
+// // listOfIngredients.insertAdjacentHTML('beforeend',markup)
+
+const markup=images.map(({url,alt})=>
+ `<li><img src="${url}" alt="${alt}" class="image"></li>`).join('')
+ console.log(markup);
+  //   const imageItem=document.createElement('img');
+
+  //         imageItem.src=image.url;
+  //         imageItem.alt=image.alt;
+  //         console.log(imageItem);
+  //    return imageItem;
+  // });
+   
+
+  listOfImage.insertAdjacentHTML('beforeend',markup);
