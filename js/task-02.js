05351140b59646922913f4fd072d1d5d03fbc63c
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-08 23:39:55 +0200

    08.12

--- js/task-02.js
+++ js/task-02.js
@@ -9,19 +9,7 @@ const ingredients = [
 
 const listOfIngredients=document.querySelector('#ingredients');
 
-// let markup='';
-// console.log(markup);
-
-// console.log(ingredients.length);
-// for (let i = 0; i < ingredients.length; i+=1) {
-// markup += `<li>${ingredients[i]}</li>`
- 
-// }
-
-// console.log(markup);
-// listOfIngredients.insertAdjacentHTML('beforeend',markup)
-
- const element=ingredients.map(ingredient=>{
+const element=ingredients.map(ingredient=>{
    const liElement=document.createElement('li');
          liElement.textContent=ingredient;
          liElement.classList.add('item');
